256d2ffd82756566a5f6b096036db7664d60d38a
Bruno Amorim Oliveira <brunoamorim1706@gmail.com>
2023-06-02 21:35:16 -0300

    html

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Bruno Amorim'
     })
-
-
-def contato(request):
-    return HttpResponse('Liga para 0800')
-
-
-def sobre(request):
-    return HttpResponse('Somos empresa de marca de roupa')
